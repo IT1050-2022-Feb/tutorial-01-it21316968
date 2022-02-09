8157d7dd1c4c1614e0956f04dc5327f7e60a6185
it21316968 <99314972+it21316968@users.noreply.github.com>
2022-02-09 23:36:37 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+
+  float num_1,num_2,num_3; //Declaring variables
+
+  printf("Enter the 1st subject marks:");//getting iputs
+  scanf("%f",&num_1);
+
+  printf("Enter the 2nd subject marks:");
+  scanf("%f",&num_2);
+
+  num_3=(num_1+num_2)/2;//calculation
+
+  printf("The average of the two marks: %.2f",num_3);//getting output
+
   
   return 0;
 }
